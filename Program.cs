f340aed2b540c64ade5bd60335ac6c8b39b541e2
C.M.D. Rogers <126674546+2652660@users.noreply.github.com>
2023-10-24 16:43:22 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        dynamic Locker = new ProtoType();
+        Locker.Test = "Hello, World!";
+        Locker.Ask = new ProtoType();
+        Locker.Ask.To = new ProtoType();
+        Locker.Ask.To.A = "1";
+        Locker.Ask.To.B = 2;
+        Locker.Ask.To.C = ProtoRune<BigInteger>.Fraction("-999999999999999999.99999999999999999");
+        Locker.Ask.For = "name";
+        Locker.Ask.Question = $"What is your {Locker.Ask.For}?";
+        Console.WriteLine(Locker.Ask);
+        Console.WriteLine(Locker.Ask.To);
+        Console.WriteLine(Locker.Ask.To.A);
+        Console.WriteLine(Locker.Ask.To.B);
+        Console.WriteLine(Locker.Ask.For);
+        Console.WriteLine(Locker.Ask.Question);
+        Console.WriteLine(ProtoType.ToJSON(Locker));
+        Console.ReadLine();
+    }
+}
